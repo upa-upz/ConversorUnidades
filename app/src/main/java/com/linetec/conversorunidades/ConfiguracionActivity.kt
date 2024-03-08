c262f78333dc700935a804b84bb10e3b74ce4f23
Santiago Fernandez <santy.fernandito@gmail.com>
2024-03-08 13:54:37 -0300

    Se agrego funcionalidad inicial configuracion

--- app/src/main/java/com/linetec/conversorunidades/ConfiguracionActivity.kt
+++ app/src/main/java/com/linetec/conversorunidades/ConfiguracionActivity.kt
@@ -9,52 +9,105 @@ import android.widget.Adapter
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.Spinner
+import android.widget.Switch
+import android.widget.Toast
 
 class ConfiguracionActivity : AppCompatActivity() {
 
-    private lateinit var spinner:Spinner
-    private lateinit var adapter:ArrayAdapter<String>
+    private lateinit var spinner: Spinner
+    private lateinit var adapter: ArrayAdapter<String>
 
-    val idiomas = arrayListOf<String>("Español","Ingles","Portuges")
+    private lateinit var switch: Switch
+
+    var leguajeSeleccionado: String = ""
+    var leguajeApp: String = ""
+    var position:Int = 0
+
+    var modoOscuro:Boolean = false
+    var idioma:String = "0"
+
+
+    val idiomas = arrayListOf<String>("Español", "Ingles", "Portuges")
+    private lateinit var btnVover: Button
+    //private lateinit var modoNoche:Switch
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuracion)
 
-        val sharedPreferences = getSharedPreferences("idiomas", Context.MODE_PRIVATE)
-
-        val valorRecuperado = sharedPreferences.getString("clave", "valorPredeterminado")
-
+        btnVover = findViewById<Button>(R.id.btn_config_volver)
+        switch = findViewById(R.id.sw_config)
         spinner = findViewById(R.id.sp_config)
-        adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,idiomas)
+        adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, idiomas)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
 
-        spinner.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+        //switch = findViewById(R.id.sw_config)
+
+        cargarDatos()
+
+        switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked){
+                modoOscuro = true
+            }else{
+                modoOscuro = false
+            }
+            guardarModoNoche(modoOscuro)
+        }
+
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
             override fun onItemSelected(
                 parent: AdapterView<*>?,
                 view: View?,
                 position: Int,
                 id: Long
             ) {
-                val editor = sharedPreferences.edit()
-                if (parent != null) {
-                    editor.putString("idioma", parent.selectedItem.toString())
-                }
-                editor.apply()
+                guardarLenguaje(position.toString())
+                Toast.makeText(this@ConfiguracionActivity,idiomas[position],Toast.LENGTH_SHORT).show()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {}
-
         }
-
-
-        val btnVover = findViewById<Button>(R.id.btn_config_volver)
 
         btnVover.setOnClickListener {
-            val intent= Intent(this,MainActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun cargarDatos() {
+        // Cargar Idioma
+        var sharedPreferences = getSharedPreferences("idioma", Context.MODE_PRIVATE)
+        val idiomaActual = sharedPreferences.getString("idioma", "")
+        val idioma:Int
+        if(idiomaActual == null){
+            idioma = 0
+        }else {
+            idioma = idiomaActual.toInt()
+        }
+        spinner.setSelection(idioma)
+
+        // Cargar Modo Nocturno
+        sharedPreferences = getSharedPreferences("modoNoche",Context.MODE_PRIVATE)
+        var modoNoche = sharedPreferences.getBoolean("modoNoche",false)
+        switch.setChecked(modoNoche)
+    }
+    private fun guardarLenguaje(value:String) {
+        idioma = value
+        val sharedPreferences = getSharedPreferences("idioma", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("idioma", idioma)
+        editor.apply()
+    }
+    private fun guardarModoNoche(value:Boolean){
+        modoOscuro = value
+        val sharedPreferences = getSharedPreferences("modoNoche",Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("modoNoche",modoOscuro)
+        editor.apply()
     }
 }
